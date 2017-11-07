483fa71243e4a914fe496f0aee077b9e1374d6ce
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-11-08 02:36:46 +0530

    Implement a chart.

---
+++ components/timeseries.js
@@ -0,0 +1,86 @@
+export default class TimeSeries extends React.Component {
+
+  renderAxis() {
+    const {
+      width,
+      height,
+      outerPadding:op
+    } = this.props
+
+    return [
+      <line key="y-axis" x1={op} y1={op} x2={op} y2={height-op} stroke="gray" strokeWidth={3}/>,
+      <line key="x-axis" x1={op} y1={height-op} x2={width-op} y2={height-op} stroke="gray" strokeWidth={3}/>
+    ]
+  }
+
+  renderChart () {
+    const {
+      width,
+      height,
+      outerPadding:op,
+      data,
+      xScale=50,
+      dotR=3,
+      lineW=1
+    } = this.props
+
+    const drawingHeight = height - (op * 2)
+    const drawingWidth = width - (op * 2)
+    const maxY = Math.max(...(data.map(i => i.y)))
+    const yScale = Math.floor(drawingHeight / maxY)
+    const drawableData = data.slice(data.length - Math.ceil(drawingWidth/xScale))
+
+    const points = [];
+    const lines = [];
+    let prevItem = null
+
+    const calcX = (val) => (width - op - (xScale * val))
+    const calcY = (val) => (height - op - (yScale * val))
+
+    for (let lc=0; lc<drawableData.length; lc++) {
+      const item = drawableData[drawableData.length - lc - 1];
+      points.push(
+        <circle
+          key={`c-${lc}`}
+          r={dotR}
+          cx={calcX(lc)}
+          cy={calcY(item.y)}
+          fill="gray"
+        />
+      )
+
+      if (prevItem !== null) {
+        lines.push(
+          <line
+            key={`l-${lc}`}
+            x1={calcX(lc-1)}
+            y1={calcY(prevItem.y)}
+            x2={calcX(lc)}
+            y2={calcY(item.y)}
+            stroke="gray"
+            strokeWidth={lineW}
+          />
+        )
+      }
+
+      prevItem = item
+    }
+
+    return [...lines, ...points]
+  }
+
+  render() {
+    const {
+      width,
+      height
+    } = this.props
+
+    return (
+      <svg width={width} height={height}>
+        {this.renderAxis()}
+        {this.renderChart()}
+      </svg>
+    )
+  }
+}
+
---
+++ pages/index.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import TimeSeries from '../components/timeseries';
+
+const data = [];
+for (let lc=0; lc<1000; lc++) {
+  data.push({
+    y: getRandom(),
+    x: `x-${lc}`
+  })
+}
+
+function getRandom () {
+  return 25 + (Math.ceil(Math.random() * 5))
+}
+
+function genData () {
+  data.unshift()
+  data.push({
+    y: getRandom(),
+    x: `x`
+  })
+  return data
+}
+
+export default class extends React.Component {
+  constructor(...args) {
+    super(...args)
+    this.state = { data: [] }
+  }
+
+  componentDidMount() {
+    this.timeoutHandler = setInterval(() => {
+      this.setState({ data: genData() })
+    }, 500);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutHandler);
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div className="wrapper">
+        <TimeSeries
+          width={1200}
+          height={500}
+          outerPadding={50}
+          data={data}
+          xScale={10}
+          dotR={3}
+          lineW={2}
+        />
+        <style jsx>{`
+          .wrapper {
+            text-align: center;
+            margin-top: 150px;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+
